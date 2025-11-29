b42c070c8b2c26a1eca167439d56dc0fad44db77
Kanan Sultanov <konansulx@gmail.com>
2025-11-30 02:44:06 +0400

    Adding simple dictionary database  to store person and his embedding

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import torch
 import numpy as np
 from ultralytics import YOLO
+from insightface.app import FaceAnalysis
+from xyzservices import providers
 
 yolo = YOLO('models/yolov12s-face.pt')
 
@@ -11,6 +13,18 @@ else:
     device = 'cpu'
 
 yolo.to(device)
+
+face_recognizer_model = FaceAnalysis(name = 'buffalo_l', providers = ['CPUExecutionProvider'])
+face_recognizer_model.prepare(ctx_id = 0)
+
+database = { }
+
+def add_person(person_name, image_path):
+    face = face_recognizer_model.get(cv2.imread(image_path))
+    database[person_name] = face[0].normed_embedding
+    print('Added:', person_name)
+
+add_person('Kanan', 'images/kanan.jpeg')
 
 cap = cv2.VideoCapture(0)
 
