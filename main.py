dfdd44a8d433ea59068c59c79ad4bbe11479e770
Kanan Sultanov <konansulx@gmail.com>
2025-12-01 18:03:11 +0400

    Implement assign_id_to_boxes function for iou based face tracking
    The function matches new YOLO bounding boxes with previous frame boxes using intersection over union. Each matched box inherits its previous ID, providing stable identity tracking. New IDs are automatically generated when no match is found. used_previous set prevents assigning the same previous ID to multiple new boxes.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import torch
+import queue
+import threading
 import numpy as np
 from ultralytics import YOLO
 from insightface.app import FaceAnalysis
 
 THRESHOLD = 0.6
+IOU_THRESHOLD = 0.3
 
 yolo = YOLO('models/yolov12s-face.pt')
 
@@ -26,6 +29,54 @@ add_person('Garnacho', 'images/alejandro_garnacho.jpeg')
 add_person('E.Fernandez', 'images/enzo_fernandez.jpeg')
 add_person('Caicedo', 'images/moises_caicedo.jpg')
 add_person('Cucurella', 'images/marc_cucurella.jpg')
+
+arc_queue = queue.Queue(maxsize = 1)
+name_results = { }
+next_id = 0
+
+def assign_id_to_boxes(new_box, prev_box):
+
+    assigned = { }
+    global next_id
+    used_previous = set()
+
+    for box in new_box:
+        x1, y1, x2, y2 = box
+
+        best_iou = 0
+        best_prev_id = None
+
+        for prev_id, (prev_x1, prev_y1, prev_x2, prev_y2) in prev_box.items():
+            if old_id in used_previous
+                continue
+
+            intersection_x1 = max(x1, prev_x1)
+            intersection_y1 = max(y1, prev_y1)
+            intersection_x2 = min(x2, prev_x2)
+            intersection_y2 = min(y2, prev_y2)
+
+            if intersection_x2 <= intersection_x1 or intersection_y2 <= intersection_y1:
+                continue
+
+            intersection_area = (intersection_x2 - intersection_x1) * (intersection_y2 - intersection_y1)
+            area_new = (x2 - x1) * (y2 - y1)
+            area_prev = (prev_x2 - prev_x1) * (prev_y2 - prev_y1)
+
+            intersection_over_union = intersection_area / (area_new + area_prev - intersection_area)
+
+            if intersection_over_union > best_iou:
+                best_iou = intersection_over_union
+                best_prev_id = prev_id
+
+            if best_prev_id is not None and best_iou > IOU_THRESHOLD:
+                assigned[best_prev_id] = box
+                used_previous.add(prev_id)
+
+            else:
+                assigned[next_id] = box
+                next_id = next_id + 1
+
+    return assigned
 
 def recognize_face(frame):
     yolo_results = yolo.predict(frame, verbose = False)
